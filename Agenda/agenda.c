dafe2a7e095c14015fb4cf54d0e34928cee2ebb3
Alana Schwendler <alanaschwendler27@gmail.com>
2017-05-18 13:27:22 -0300

    Agenda está inserindo

--- Agenda/agenda.c
+++ Agenda/agenda.c
@@ -2,6 +2,18 @@
 #include <stdlib.h>
 #include <string.h>
 
+/*
+Faça uma agenda capaz de incluir, apagar, buscar
+e listar quantas pessoas o usuário desejar, porém,
+você não pode usar variáveis a não ser ponteiros.
+Toda a informação deve ser guardada num “void
+*pBuffer”.
+
+pBuffer deve guardar desde as pessoas colocadas
+na agenda até as variáveis locais que você precisa
+para controlar um for, por exemplo.
+*/
+
 typedef struct Pessoa { 
 	char nome[20];
 	int idade;
@@ -11,23 +23,36 @@ int main() {
 	void *pBuffer;
 	int *qt;
 	int *opcao;
-	char *nomeAux;
-	int *idadeAux;
-	Pessoa *p;
+	Pessoa *pInicio;
+	Pessoa *pAtual;
  
-	pBuffer = malloc(sizeof(int)); 
+	pBuffer = malloc(sizeof(int)); //aloca tamanho de um inteiro pra colocar a quantidade
 	qt = pBuffer; //quantidade de pessoas inseridas
-	qt = 0; //seta como 0
+	*qt = 0; //seta como 0
 
-	pBuffer = realloc(pBuffer, (pBuffer + sizeof(int))); //realoca espaço pra adicionar a opção
+	pBuffer = realloc(pBuffer, (sizeof(int)*2)); //realoca espaço pra adicionar a opção
 	opcao = (qt + sizeof(int)); //opção aponta pra qt + 4
+	qt = pBuffer; //reajusta o ponteiro de qt
 
-	pBuffer = realloc(pBuffer, (pBuffer + sizeof(char)*20)); //aloca espaço pra adicionar o nome
-	nomeAux = (opcao + sizeof(char)*20); //ajusta ponteiro do nome
+	pBuffer = realloc(pBuffer, (sizeof(Pessoa))); //realoca espaço pra adicionar uma pessoa
+	qt = pBuffer; //reajusta o ponteiro de qt
+	pInicio = (Pessoa *) (opcao + sizeof(int));
+	pAtual = pInicio; 
 
-	pBuffer = realloc(pBuffer, (pBuffer + sizeof(int))); //aloca espaço pra adicionar a idade
-	idadeAux = (nomeAux + sizeof(int)); //ajusta ponteiro da idade
+	printf("1 - Inserir\n");
+	scanf("%d", opcao); //le qual oção o usuario vai querer
 
+	if(*opcao == 1) {
+		*qt = *qt + 1; //se a opção foi de inserir, adiciona mais um na quantidade
+		scanf("%s", (*pAtual).nome);
+		printf("\n");
+		scanf("%d", &(*pAtual).idade);
+	}
+
+	printf("Nome: %s\n", pAtual->nome);
+	printf("Idade: %d\n", pAtual->idade);
+
+	free(pBuffer);
 
 	return 0;
 }
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-#Código da agenda de AED
-Inicialmente, aloco espaço pro meu ponteiro de void, pBuffer
+#Código da agenda de AED  
+Inicialmente, aloco espaço pro meu ponteiro de void, pBuffer 
 Depois, vou alocando mais espaço pra adicionar as variáveis que serão usadas do decorrer do programa
 A struct Pessoa tem tamanho 24, que é o tamanho de 20 pro nome e 4 pra idade
 
