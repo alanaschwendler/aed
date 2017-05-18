514ca6ce2896930f81613f49cdff7d52df225167
Alana Schwendler <alanaschwendler27@gmail.com>
2017-05-18 14:39:19 -0300

    Insere e lista

--- Agenda/agenda.c
+++ Agenda/agenda.c
@@ -23,6 +23,7 @@ int main() {
 	void *pBuffer;
 	int *qt;
 	int *opcao;
+	int *x; //controle do for 
 	Pessoa *pInicio;
 	Pessoa *pAtual;
  
@@ -34,23 +35,35 @@ int main() {
 	opcao = (qt + sizeof(int)); //opção aponta pra qt + 4
 	qt = pBuffer; //reajusta o ponteiro de qt
 
+	pBuffer = realloc(pBuffer, (sizeof(int)*3));
+	qt = pBuffer;
+	x = (opcao + sizeof(int));
+
 	pBuffer = realloc(pBuffer, (sizeof(Pessoa))); //realoca espaço pra adicionar uma pessoa
 	qt = pBuffer; //reajusta o ponteiro de qt
-	pInicio = (Pessoa *) (opcao + sizeof(int));
+	pInicio = (Pessoa *) (x + sizeof(int));
 	pAtual = pInicio; 
 
 	printf("1 - Inserir\n");
-	scanf("%d", opcao); //le qual oção o usuario vai querer
-
-	if(*opcao == 1) {
-		*qt = *qt + 1; //se a opção foi de inserir, adiciona mais um na quantidade
-		scanf("%s", (*pAtual).nome);
-		printf("\n");
-		scanf("%d", &(*pAtual).idade);
-	}
-
-	printf("Nome: %s\n", pAtual->nome);
-	printf("Idade: %d\n", pAtual->idade);
+	printf("2 - Listar Pessoas\n");
+	printf("0 - Sair\n");
+	
+	do {
+		printf("Opção: ");
+		scanf("%d", opcao); //le qual oção o usuario vai querer
+		if(*opcao == 1) {
+			*qt = *qt + 1; //se a opção foi de inserir, adiciona mais um na quantidade
+			scanf("Insira o nome: %s", (*pAtual).nome);
+			printf("\n");
+			scanf("Insira a idade: %d", &(*pAtual).idade);
+		} else if(*opcao == 2) {
+			for(*x = 0; *x < *qt; (*x)++) {
+				printf("Nome: %s\n", pAtual->nome);
+				printf("Idade: %d\n", pAtual->idade);
+				printf("------\n");
+			}
+		}
+	} while(*opcao != 0);
 
 	free(pBuffer);
 
