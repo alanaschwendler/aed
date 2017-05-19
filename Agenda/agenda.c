34869cec07e205a7ea12ae51c7ecfdf18b32be83
alanaschwendler <alanaschwendler27@gmail.com>
2017-05-19 01:08:41 -0300

    Agora a agenda insere, lista e busca, porém tem alguma coisa indevida escrevendo na memória

--- Agenda/agenda.c
+++ Agenda/agenda.c
@@ -2,7 +2,112 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct Pessoa { 
+	char nome[20];
+	int idade;
+} Pessoa;
+
+void insere(Pessoa *pAtual);
+void imprime(Pessoa *pAtual);
+void busca(char *nomeAux, Pessoa *pAtual, Pessoa *pInicio, int *qt, int *x);
+
+int main() {
+	void *pBuffer;
+	int *qt;
+	int *opcao;
+	int *x; //controle do for 
+	char *nomeAux;
+	Pessoa *pInicio;
+	Pessoa *pAtual;
+ 
+	/*
+	pBuffer = malloc(sizeof(int)); //aloca tamanho de um inteiro pra colocar a quantidade
+	qt = pBuffer; //quantidade de pessoas inseridas
+
+	pBuffer = realloc(pBuffer, (sizeof(int)*2)); //realoca espaço pra adicionar a opção
+	qt = pBuffer; //reajusta o ponteiro de qt
+	opcao = (qt + sizeof(int)); //opção aponta pra qt + 4
+	*/
+
+	pBuffer = malloc(sizeof(int)*3); //aloca memória pros ints e pro nome auxiliar(usado na busca)
+	pBuffer = realloc(pBuffer, sizeof(int)*3 + sizeof(char)*20);
+	qt = pBuffer;
+	*qt = 0; //seta como 0
+	opcao = (qt + sizeof(int)); //opção aponta pra qt + 4	
+	x = (opcao + sizeof(int));
+	nomeAux = (char *)(x + sizeof(int));
+
+	printf("1 - Inserir Pessoa\n");
+	printf("2 - Listar Pessoas\n");
+	printf("3 - Buscar Pessoa\n");
+	printf("0 - Sair\n");
+	
+	do {
+		printf("Opção: ");
+		scanf("%d", opcao); //le qual oção o usuario vai querer
+
+		if(*opcao == 1) {
+			*qt = *qt + 1; //se a opção foi de inserir, adiciona mais um na quantidade
+			
+			pBuffer = realloc(pBuffer, (sizeof(int)*3) + (sizeof(char)*20) + (sizeof(Pessoa) * (*qt))); //realoca espaço de acordo com a qt de pessoas //  + (sizeof(char)*20)
+			qt = pBuffer; //reajusta o ponteiro de qt
+			opcao = (qt + sizeof(int)); //opção aponta pra qt + 4
+			x = (opcao + sizeof(int)); //x aponta pra opção + 4
+			nomeAux = (char *)(x + sizeof(int));
+			pInicio = (Pessoa *) (nomeAux + (sizeof(char) * 20));//(nomeAux + sizeof(char)*20);
+
+			pAtual = pInicio + (sizeof(Pessoa) * ((*qt)-1)); //agora a pessoa atual aponta pro inicio da memória alocada pra próxima pessoa
+
+			insere(pAtual);
+		} else if(*opcao == 2) {
+			pAtual = pInicio;
+			for(*x = 0; *x < *qt; (*x)++) {
+				imprime(pAtual);
+				pAtual = pAtual + sizeof(Pessoa);
+			}
+		} else if(*opcao == 3) {
+			printf("Quem você deseja buscar? ");
+			scanf("%s", nomeAux);
+			busca(nomeAux, pAtual, pInicio, qt, x);
+		}
+	} while(*opcao != 0);
+
+	free(pBuffer);
+
+	return 0;
+}
+
+void insere(Pessoa *pAtual) {
+	printf("Insira o nome: ");
+	scanf("%s", (*pAtual).nome);
+	printf("Insira a idade: ");
+	scanf("%d", &((*pAtual).idade));
+}
+
+void imprime(Pessoa *pAtual) {
+	printf("Nome: %s\n", pAtual->nome);
+	printf("Idade: %d\n", (*pAtual).idade);
+	printf("------\n");
+}
+
+void busca(char *nomeAux, Pessoa *pAtual, Pessoa *pInicio, int *qt, int *x) {
+	pAtual = pInicio;
+	for(*x = 0; *x < *qt; (*x)++) {
+		if(strcmp(pAtual->nome, nomeAux) == 0) {
+			printf("Encontrado\n");
+			printf("Nome: %s\n", pAtual->nome);
+			printf("Idade: %d\n", (*pAtual).idade);
+		} else {
+			printf("Não encontrado.\n");
+		}
+		pAtual = pAtual + sizeof(Pessoa);
+	}
+}
+
+
 /*
+PROPOSTA DA AGENDA
+
 Faça uma agenda capaz de incluir, apagar, buscar
 e listar quantas pessoas o usuário desejar, porém,
 você não pode usar variáveis a não ser ponteiros.
@@ -13,59 +118,3 @@ pBuffer deve guardar desde as pessoas colocadas
 na agenda até as variáveis locais que você precisa
 para controlar um for, por exemplo.
 */
-
-typedef struct Pessoa { 
-	char nome[20];
-	int idade;
-} Pessoa;
-
-int main() {
-	void *pBuffer;
-	int *qt;
-	int *opcao;
-	int *x; //controle do for 
-	Pessoa *pInicio;
-	Pessoa *pAtual;
- 
-	pBuffer = malloc(sizeof(int)); //aloca tamanho de um inteiro pra colocar a quantidade
-	qt = pBuffer; //quantidade de pessoas inseridas
-	*qt = 0; //seta como 0
-
-	pBuffer = realloc(pBuffer, (sizeof(int)*2)); //realoca espaço pra adicionar a opção
-	opcao = (qt + sizeof(int)); //opção aponta pra qt + 4
-	qt = pBuffer; //reajusta o ponteiro de qt
-
-	pBuffer = realloc(pBuffer, (sizeof(int)*3));
-	qt = pBuffer;
-	x = (opcao + sizeof(int));
-
-	pBuffer = realloc(pBuffer, (sizeof(Pessoa))); //realoca espaço pra adicionar uma pessoa
-	qt = pBuffer; //reajusta o ponteiro de qt
-	pInicio = (Pessoa *) (x + sizeof(int));
-	pAtual = pInicio; 
-
-	printf("1 - Inserir\n");
-	printf("2 - Listar Pessoas\n");
-	printf("0 - Sair\n");
-	
-	do {
-		printf("Opção: ");
-		scanf("%d", opcao); //le qual oção o usuario vai querer
-		if(*opcao == 1) {
-			*qt = *qt + 1; //se a opção foi de inserir, adiciona mais um na quantidade
-			scanf("Insira o nome: %s", (*pAtual).nome);
-			printf("\n");
-			scanf("Insira a idade: %d", &(*pAtual).idade);
-		} else if(*opcao == 2) {
-			for(*x = 0; *x < *qt; (*x)++) {
-				printf("Nome: %s\n", pAtual->nome);
-				printf("Idade: %d\n", pAtual->idade);
-				printf("------\n");
-			}
-		}
-	} while(*opcao != 0);
-
-	free(pBuffer);
-
-	return 0;
-}
