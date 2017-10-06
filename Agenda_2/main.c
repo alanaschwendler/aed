6f8706c0bca55b7f52d8f4b60da6e7dda001d8e1
Alana Schwendler <aschwendler@inf.ufpel.edu.br>
2017-10-06 16:43:31 -0300

    Inicio da agenda
    Agenda sendo feita com struct no buffer unico. Versão 2017/02

---
+++ Agenda_2/main.c
@@ -0,0 +1,88 @@
+//Alana Schwendler - M2
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct p {
+	char nome[20];
+	int idade;
+} Pessoa;
+
+void inserir(Pessoa *p);
+void imprimir(Pessoa *p);
+
+int main() {
+	void *pBuffer;	//buffer
+	pBuffer = NULL;
+
+	int *qt;		//quantidade de contatos no buffer
+	int *opcao;		//opção desejada
+	int *x;
+	Pessoa *inicio; //primeira pessoa no buffer
+	Pessoa *atual;	//pessoa em questão 
+
+	if(pBuffer == NULL) {
+		pBuffer = malloc((sizeof(Pessoa)) + (sizeof(int) *3));	//aloca espaço pra uma pessoa e dois inteiros
+	} else {
+		printf("**Erro: Memória insuficiente**\n");
+		return 1; //se não conseguir alocar memória, encerra o programa
+	}
+
+	qt = (int *)pBuffer; 	//qt aponta pro inicio do buffer
+	*qt = 0; 				//quantidade é zero porque não tem ninguém na agenda
+	opcao = qt + 1;	//opção aponta pra posição depois da quantidade
+	x = opcao + 1; //x aponta pra posição depois da opção
+	inicio = (Pessoa *) x + 1; //inicio aponta pra depois do x
+	atual = inicio; 	//na primeira vez, a pessoa inicial e atual são a mesma
+
+	printf("1 - Inserir \n2 - Listar \n3 - Remover \n0 - Sair \n");
+	
+	do {
+		printf("Opcao: ");
+		scanf("%d", opcao);	//le a opção
+
+		if(*opcao == 1) {
+			*qt = *qt +1;	//incrementa a quantidade
+			pBuffer = realloc(pBuffer, (sizeof(int) * 3) + sizeof(Pessoa) * (*qt));	//realoca espaço pro buffer com a nova quantidade
+			qt = (int *)pBuffer; 	//reajusta o ponteiro da quantidade
+			opcao = qt + 1;	//reajusta o ponteiro da opção
+			x = opcao + 1; //reajusta o ponteiro do x
+			*x = 0;
+			inicio = (Pessoa *)x + 1;	//reajusta o ponteiro da pessoa inicial
+
+			if(*qt == 1) {
+				inserir(inicio);	//chama a função pra inserir pessoa no buffer passando a inicial, se for o caso
+			} else if (*qt > 1) {
+				atual = (Pessoa *)atual + sizeof(Pessoa); 	//reajusta o ponteiro da pessoa atual
+				inserir(atual);		//caso não seja a primeira pessoa a ser inserida, chama a função passando a pessoa atual
+			}
+		}
+
+		if(*opcao == 2) {
+			atual = inicio;
+			
+			for(*x = 0; *x < *qt; (*x)++) {
+				imprimir(atual);
+				atual = atual + 1; //vai andando uma pessoa por vez e imprimindo
+			}
+		}
+
+	} while(*opcao != 0);
+
+	free(pBuffer);	//libera o espaço de memória utilizado pelo buffer
+	return 0;
+}
+
+void inserir(Pessoa *p) {
+	printf("Nome: ");
+	scanf("%s", (*p).nome);
+	printf("Idade: ");
+	scanf("%d", &(*p).idade);
+}
+
+void imprimir(Pessoa *p) {
+	printf("Nome: %s\n", (*p).nome);
+	printf("Idade: %d\n", (*p).idade);
+	printf("----------\n");
+}
